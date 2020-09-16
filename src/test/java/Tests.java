f90f86147a9bd320ecf86a989766616fc86f00c5
Tassio Lima <tassiopelima@gmail.com>
2020-09-16 00:40:13 -0300

    ajustes automacao gradlew

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,8 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 public class Tests extends Hooks {
 
-
-
     @Test
     public void firstTest() {
         assertTrue(pageScreen.getValue().isDisplayed());
