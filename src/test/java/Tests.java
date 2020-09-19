8bf150e5bc22ac8a976e30b4f0fd43685ef1a9c6
Tassio Lima <tassiopelima@gmail.com>
2020-09-19 12:42:38 -0300

    adjustments

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,15 +1,19 @@
-import appium.Hooks;
-import org.junit.After;
-import org.junit.Before;
+import appium.AppiumController;
 import org.junit.Test;
 import screens.PageScreen;
 
 import static org.junit.Assert.assertTrue;
 
-public class Tests extends Hooks {
+public class Tests extends AppiumController {
+
+    private PageScreen pagescreen;
+
+    public Tests(){
+        pagescreen = new PageScreen(driver);
+    }
 
     @Test
     public void firstTest() {
-        assertTrue(pageScreen.getValue().isDisplayed());
+        assertTrue(pagescreen.getValue().isDisplayed());
     }
 }
